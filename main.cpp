ec83a733a9d27e5e831b06fc0e6f047e1707b7b6
Varga László - BZ7BTK <vlaszlo9321@gmail.com>
2026-03-02 19:26:17 +0100

    Hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -3,23 +3,32 @@
 constexpr int N_ELEMENTS = 100;
 
 int main()
-{
+{   
+    //nem definiált a használatban lévő változó
     int *b = new int[NELEMENTS];
     std::cout << '1-100 ertekek duplazasa'
+
+    //szintaktikailag hibás for loop
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+
+    //hibásan működő ciklus, rossz a feltétel
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
+
+
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        //hiányzó sorvégi ;
         atlag += b[i]
     }
+    
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
